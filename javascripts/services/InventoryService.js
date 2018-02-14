18d1309e9e053ebedc212fd8bc5205f59bfabbf0
Adam Smith <hagansmith@gmail.com>
2018-02-14 13:08:10 -0600

    enable location updates

--- javascripts/services/InventoryService.js
+++ javascripts/services/InventoryService.js
@@ -26,6 +26,17 @@ app.service("InventoryService", function ($http, $q, FIREBASE_CONFIG){
     return $http.post(`${FIREBASE_CONFIG.databaseURL}/locations.json`, JSON.stringify(location));
    };
 
-  return {getAllLocations, getAllProducts, postLocation, postProduct};
+   const putLocation = (location) => {
+     console.log(location);
+     let locationObject = {
+       "name" : location.name,
+       "pickable" : location.pickable,
+       "active" : location.active
+     };
+     let lid = location.id;
+     return $http.put(`${FIREBASE_CONFIG.databaseURL}/locations/${lid}.json`, JSON.stringify(locationObject));
+   };
+
+  return {getAllLocations, getAllProducts, postLocation, postProduct, putLocation};
 
  });
